b46648748936dcfd2fb9cf18d0d33c8742fe4f83
pierre <pierre.charlier@cit-consulting.be>
2025-10-06 12:39:20 +0000

    Add Rest Controller

--- src/main/java/com/unicorn/agent/AgentApplication.java
+++ src/main/java/com/unicorn/agent/AgentApplication.java
@@ -15,24 +15,7 @@ public class AgentApplication {
 		SpringApplication.run(AgentApplication.class, args);
 	}
 
-	    @Bean
-    public CommandLineRunner cli(ChatClient.Builder chatClientBuilder) {
-        return args -> {
-            var chatClient = chatClientBuilder
-                .defaultSystem("You are a Unicorn Rentals Agent, expert in all sorts of things related to Unicorns and renting them.")
-                .build();
+	
 
-            System.out.println("\nI am your Unicorn Rentals assistant.\n");
-            try (Scanner scanner = new Scanner(System.in)) {
-                while (true) {
-                    System.out.print("\nUSER: ");
-                    System.out.println("\nASSISTANT: " +
-                        chatClient.prompt(scanner.nextLine()) // Get the user input
-                            .call()
-                            .content());
-                }
-            }
-        };
-    }
 
 }
